76db4155dbc5fa39ad0a51a27adf550450e7c469
geversonfernandes <31553941+geversonfernandes@users.noreply.github.com>
2025-03-29 19:43:18 -0300

    Update authMiddleware.js

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -8,7 +8,7 @@ export const authMiddleware = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, 'secret')
     req.user = decoded
     next()
   } catch (error) {
